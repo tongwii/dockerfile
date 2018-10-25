99b7483c794b27a056acd01f41995ea122e8ad4b
dean.chou <dean.chou@aliyun.com>
2018-10-25 11:37:30 +0800

    update

--- shadowsocks-privoxy/rootfs/entrypoint.sh
+++ shadowsocks-privoxy/rootfs/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 env sslocal -s $SERVER_ADDR -p $SERVER_PORT -k $PASSWORD \
-  -b 0.0.0.0 -l ${LOCAL_PORT:-1080} -m ${METHOD:-'aes-256-cfb'} \
-  -d start
+  -b 0.0.0.0 -l 1080 -m ${METHOD:-'aes-256-cfb'} -d start
 
 env /usr/sbin/privoxy --no-daemon /etc/privoxy/config
